c6fb02cc14c186d8613023c2f3b4b1bd960551a2
fing9 <djajsl54@naver.com>
2023-12-03 19:20:11 +0900

    bugfix : security config dependency error fix

--- src/main/java/com/randps/randomdefence/global/config/SecurityConfig.java
+++ src/main/java/com/randps/randomdefence/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.randps.randomdefence.global.jwt.JwtRefreshUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
