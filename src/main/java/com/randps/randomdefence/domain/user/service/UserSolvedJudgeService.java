3e055152467376e0bb14ccf1f71f0f01a35fb11d
fing9 <djajsl54@naver.com>
2023-12-04 13:41:11 +0900

    feat : problem 조회시 유저가 푼 문제인지 여부 포함해서 반환

--- src/main/java/com/randps/randomdefence/domain/user/service/UserSolvedJudgeService.java
+++ src/main/java/com/randps/randomdefence/domain/user/service/UserSolvedJudgeService.java
@@ -1,5 +1,6 @@
 package com.randps.randomdefence.domain.user.service;
 
+import com.randps.randomdefence.domain.problem.dto.ProblemDto;
 import com.randps.randomdefence.domain.user.domain.UserSolvedProblem;
 import com.randps.randomdefence.domain.user.domain.UserSolvedProblemRepository;
 import java.util.Optional;
@@ -22,6 +23,15 @@ public class UserSolvedJudgeService {
     public boolean isSolved(String bojHandle, Integer problemId) {
         Optional<UserSolvedProblem> problem  = userSolvedProblemRepository.findByBojHandleAndProblemId(bojHandle, problemId);
         return problem.isPresent() || userAlreadySolvedService.isSolved(bojHandle, problemId);
+    }
+
+    /*
+     * 유저가 문제를 풀었는지 여부를 반환한다.
+     */
+    @Transactional
+    public boolean isSolved(String bojHandle, ProblemDto problemDto) {
+        Optional<UserSolvedProblem> problem  = userSolvedProblemRepository.findByBojHandleAndProblemId(bojHandle, problemDto.getProblemId());
+        return problem.isPresent() || userAlreadySolvedService.isSolved(bojHandle, problemDto.getProblemId());
     }
 
 }
