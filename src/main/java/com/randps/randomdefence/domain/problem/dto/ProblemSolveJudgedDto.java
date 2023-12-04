bbb2ac647ecd29c20ffb7dc3468515aaeac35036
fing9 <djajsl54@naver.com>
2023-12-04 15:46:19 +0900

    bugfix : Dto 롬복 에러 수정

--- src/main/java/com/randps/randomdefence/domain/problem/dto/ProblemSolveJudgedDto.java
+++ src/main/java/com/randps/randomdefence/domain/problem/dto/ProblemSolveJudgedDto.java
@@ -3,7 +3,9 @@ package com.randps.randomdefence.domain.problem.dto;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
+import lombok.Data;
 
+@Data
 public class ProblemSolveJudgedDto {
 
 
