e9236f232b953ccf458b88e543312f7b4cb13a86
fing9 <djajsl54@naver.com>
2024-01-01 18:07:39 +0900

    [REFACTOR] FakeBoardRepository Mock 레포지토리 중복 코드 함수화

--- src/test/java/com/randps/randomdefence/domain/board/mock/FakeBoardRepository.java
+++ src/test/java/com/randps/randomdefence/domain/board/mock/FakeBoardRepository.java
@@ -58,6 +58,37 @@ public class FakeBoardRepository implements BoardRepository {
         }
     }
 
+    private Page<BoardSimple> toBoardSimplePage(Pageable pageable, List<Board> filteredData) {
+        List<BoardSimple> boardSimples;
+        long total = filteredData.size();
+
+        if (pageable.getPageNumber() == 0) {
+            boardSimples = filteredData.stream().limit(pageable.getPageSize())
+                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
+                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
+                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
+                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
+                            .profileImg(
+                                    userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
+                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
+                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
+                    .collect(Collectors.toList());
+        } else {
+            boardSimples = filteredData.stream().skip((long) pageable.getPageSize() * pageable.getPageNumber())
+                    .limit(pageable.getPageSize())
+                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
+                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
+                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
+                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
+                            .profileImg(
+                                    userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
+                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
+                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
+                    .collect(Collectors.toList());
+        }
+        return new PageImpl<>(boardSimples, pageable, total);
+    }
+
     @Override
     public void delete(Board board) {
         data.removeIf(item -> item.getId().equals(board.getId()));
@@ -70,181 +101,56 @@ public class FakeBoardRepository implements BoardRepository {
 
     @Override
     public Page<BoardSimple> findAllBoardSimplePaging(Pageable pageable) {
-        List<BoardSimple> boardSimples;
-        long total = data.size();
-
-        if (pageable.getPageNumber() == 0) {
-            boardSimples = data.stream().limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        } else {
-            boardSimples = data.stream().skip((long) pageable.getPageSize() * pageable.getPageNumber())
-                    .limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        }
-        return new PageImpl<>(boardSimples, pageable, total);
+        return toBoardSimplePage(pageable, data);
     }
 
     @Override
     public Page<BoardSimple> findAllBoardSimpleByTypePaging(String type, Pageable pageable) {
-        List<BoardSimple> boardSimples;
         List<Board> filteredData = data.stream().filter(elem -> elem.getType().contains(type))
                 .collect(Collectors.toList());
-        long total = filteredData.size();
 
-        if (pageable.getPageNumber() == 0) {
-            boardSimples = filteredData.stream().limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        } else {
-            boardSimples = filteredData.stream()
-                    .skip((long) pageable.getPageSize() * pageable.getPageNumber()).limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        }
-        return new PageImpl<>(boardSimples, pageable, total);
+        return toBoardSimplePage(pageable, filteredData);
     }
 
     @Override
     public BoardDetail findBoardDetail(Long boardId) {
         return data.stream().filter(elem -> elem.getId().equals(boardId))
-                .map(elem -> BoardDetail.builder()
-                        .id(elem.getId())
-                        .createdDate(elem.getCreatedDate())
-                        .modifiedDate(elem.getModifiedDate())
-                        .type(elem.getType())
-                        .bojHandle(elem.getBojHandle())
+                .map(elem -> BoardDetail.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
+                        .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
                         .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
                         .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
                         .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                        .title(elem.getTitle())
-                        .content(elem.getContent())
-                        .problemId(elem.getProblemId())
-                        .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                .findAny().orElseThrow();
+                        .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
+                        .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build()).findAny()
+                .orElseThrow();
     }
 
     @Override
     public Page<BoardSimple> findAllUserBoardSimplePaging(String bojHandle, Pageable pageable) {
-        List<BoardSimple> boardSimples;
         List<Board> filteredData = data.stream().filter(elem -> elem.getBojHandle().contains(bojHandle))
                 .collect(Collectors.toList());
-        long total = filteredData.size();
 
-        if (pageable.getPageNumber() == 0) {
-            boardSimples = filteredData.stream().limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        } else {
-            boardSimples = filteredData.stream()
-                    .skip((long) pageable.getPageSize() * pageable.getPageNumber()).limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        }
-        return new PageImpl<>(boardSimples, pageable, total);
+        return toBoardSimplePage(pageable, filteredData);
     }
 
     @Override
     public Page<BoardSimple> findAllBoardSimpleByQueryPaging(String query, Pageable pageable) {
-        List<BoardSimple> boardSimples;
         List<Board> filteredData = data.stream().filter(elem -> elem.getTitle().contains(query))
                 .collect(Collectors.toList());
-        long total = filteredData.size();
 
-        if (pageable.getPageNumber() == 0) {
-            boardSimples = filteredData.stream().limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        } else {
-            boardSimples = filteredData.stream()
-                    .skip((long) pageable.getPageSize() * pageable.getPageNumber()).limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        }
-        return new PageImpl<>(boardSimples, pageable, total);
+        return toBoardSimplePage(pageable, filteredData);
     }
 
     @Override
     public Page<BoardSimple> findAllBoardSimpleByConditionPaging(SearchCondition condition, Pageable pageable) {
-        List<BoardSimple> boardSimples;
         List<Board> filteredData = data.stream()
-                .filter(elem -> !condition.type.isEmpty() && !condition.type.isBlank() && elem.getType().equals(condition.type))
-                .filter(elem -> !condition.bojHandle.isEmpty() && !condition.bojHandle.isBlank() && elem.getBojHandle().equals(condition.bojHandle))
-                .filter(elem -> !condition.query.isEmpty() && !condition.query.isBlank() && elem.getTitle().contains(condition.query))
-                .collect(Collectors.toList());
-        long total = filteredData.size();
+                .filter(elem -> !condition.type.isEmpty() && !condition.type.isBlank() && elem.getType()
+                        .equals(condition.type))
+                .filter(elem -> !condition.bojHandle.isEmpty() && !condition.bojHandle.isBlank() && elem.getBojHandle()
+                        .equals(condition.bojHandle))
+                .filter(elem -> !condition.query.isEmpty() && !condition.query.isBlank() && elem.getTitle()
+                        .contains(condition.query)).collect(Collectors.toList());
 
-        if (pageable.getPageNumber() == 0) {
-            boardSimples = filteredData.stream().limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        } else {
-            boardSimples = filteredData.stream().skip((long) pageable.getPageSize() * pageable.getPageNumber())
-                    .limit(pageable.getPageSize())
-                    .map(elem -> BoardSimple.builder().id(elem.getId()).createdDate(elem.getCreatedDate())
-                            .modifiedDate(elem.getModifiedDate()).type(elem.getType()).bojHandle(elem.getBojHandle())
-                            .notionId(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getNotionId())
-                            .emoji(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getEmoji())
-                            .profileImg(userRepository.findByBojHandle(elem.getBojHandle()).orElseThrow().getProfileImg())
-                            .title(elem.getTitle()).content(elem.getContent()).problemId(elem.getProblemId())
-                            .commentCount((long) commentRepository.findAllByBoardId(elem.getId()).size()).build())
-                    .collect(Collectors.toList());
-        }
-        return new PageImpl<>(boardSimples, pageable, total);
+        return toBoardSimplePage(pageable, filteredData);
     }
 }
