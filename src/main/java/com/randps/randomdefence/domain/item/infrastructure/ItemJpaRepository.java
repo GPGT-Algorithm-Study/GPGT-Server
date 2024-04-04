46dc79d3733913b1c95490f3fb9249f36f50878e
fing9 <djajsl54@naver.com>
2024-04-04 14:40:37 +0900

    [BUGFIX] item에 비관적 락 적용

--- src/main/java/com/randps/randomdefence/domain/item/infrastructure/ItemJpaRepository.java
+++ src/main/java/com/randps/randomdefence/domain/item/infrastructure/ItemJpaRepository.java
@@ -1,7 +1,16 @@
 package com.randps.randomdefence.domain.item.infrastructure;
 
 import com.randps.randomdefence.domain.item.domain.Item;
+import java.util.Optional;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.lang.NonNull;
 
 public interface ItemJpaRepository extends JpaRepository<Item, Long> {
+
+  @NonNull
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  Optional<Item> findById(@NonNull Long id);
+
 }
