3211595f17a91bc61b831e39a72584564670cf95
fing9 <djajsl54@naver.com>
2024-04-23 16:57:00 +0900

    [Fix] complaint조회 createdDate 추가

--- src/main/java/com/randps/randomdefence/domain/complaint/dto/ComplaintDetailResponse.java
+++ src/main/java/com/randps/randomdefence/domain/complaint/dto/ComplaintDetailResponse.java
@@ -3,6 +3,7 @@ package com.randps.randomdefence.domain.complaint.dto;
 import com.randps.randomdefence.domain.complaint.domain.Complaint;
 import com.randps.randomdefence.domain.complaint.enums.ComplaintType;
 import com.randps.randomdefence.domain.complaint.enums.ProcessType;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.Builder;
@@ -26,6 +27,8 @@ public class ComplaintDetailResponse {
 
   private String reply;
 
+  private LocalDateTime createdDate;
+
   public static ComplaintDetailResponse from(Complaint complaint) {
     return ComplaintDetailResponse.builder()
         .id(complaint.getId())
@@ -35,6 +38,7 @@ public class ComplaintDetailResponse {
         .processType(complaint.getProcessType())
         .processor(complaint.getProcessor())
         .reply(complaint.getReply())
+        .createdDate(complaint.getCreatedDate())
         .build();
   }
 
