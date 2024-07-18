9f1634b019aab28e2305a1f3e2d53221588e38c8
fing9 <djajsl54@naver.com>
2024-07-18 19:30:44 +0900

    [FEAT] Retryable을 이용한 Scheduler 메서드 실패시 재시도 로직 추가

--- src/main/java/com/randps/randomdefence/RandomdefenceApplication.java
+++ src/main/java/com/randps/randomdefence/RandomdefenceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
 @SpringBootApplication(
